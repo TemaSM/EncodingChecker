cc513b9e02c00054f7309de5193a5233e55cdeb6
Tema Smirnov <git.tema@smirnov.one>
2021-02-26 23:02:02 +0300

    added temp Sentry crash/errors logging

--- sources/EncodingChecker/Program.cs
+++ sources/EncodingChecker/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
+using SharpRaven;
 
 namespace EncodingChecker
 {
     internal static class Program
     {
+        public static RavenClient ravenClient;
         [STAThread]
         private static void Main()
         {
             Application.ThreadException += OnApplicationThreadException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ravenClient = new RavenClient("https://09c4c97e7db24bc99ea8b59d5bb484d5@o535078.ingest.sentry.io/5654022");
             Application.Run(new MainForm());
         }
 
